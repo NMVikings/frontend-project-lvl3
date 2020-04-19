e329a23f1b19ce3d8e27ac06a5b1dd258ea07172
Egor <no.more.vikings@gmail.com>
2020-04-19 19:45:09 +0300

    Remove async await from tests

--- __tests__/init.test.js
+++ __tests__/init.test.js
@@ -2,16 +2,16 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import init from '../src/init';
 
-const setupDocument = async () => {
+const setupDocument = () => {
   const pathToHtml = path.resolve(__dirname, '__fixtures__/index.html');
-  const html = await fs.readFile(pathToHtml, 'utf8');
+  const html = fs.readFileSync(pathToHtml, 'utf8');
   document.body.innerHTML = html;
 };
 
 describe('init', () => {
-  it('init', async () => {
+  it('init', () => {
     expect.assertions(1);
-    await setupDocument();
+    setupDocument();
     init();
     expect(true).toBeDefined();
   });
